be1ada01259b454ce27f7ecee059c18ac0a0b823
hungditga88 <taolaovjp888@gmail.com>
2022-12-04 14:49:44 +0700

    d

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const fruit = [["Grape",15],["Banana",12],["Apple",2],["Watermelon",1],["Kiwi",5],["Durian",3]];
-for(let j = 0; j < fruit.length; j++){
-    let Number = fruit[j].length;
-    for(let i = 0; i < Number; i++){
-        console.log("I have" + " " + fruit[j] + fruit[i] + " ");
-    }
+// Bài 1
+const fruits = [["Grape",15],["Banana",12],["Apple",2],["Watermelon",1],["Kiwi",5],["Durian",3]];
+for (let i = 0; i < fruits.length; i++){
+        console.log("I have" + " " + fruits[i][1] + " " + fruits[i][0]);
 }
+
+
+// Bài 2
+// Vòng lập for
+for (let h = 0; h < fruits.length; h++){
+    console.log(fruits[h][1] + " " + fruits[h][0])
+}
+// Vòng lập for of
+for (let fruit of fruits){
+    console.log(fruit[1] + " " + fruit[0])
+}
+// Vòng lập for in
+for (fruit in fruits){
+    console.log(fruit)
+}
+
+// Bài 3
+var myDog = {
+    "name": "Ngáo",
+    "legs": "4",
+    "friends": ["everything!"],
+}
+myDog.name = "Husky"
+myDog.color = 'Brown';
+delete myDog["friends"];
+console.log(myDog);
